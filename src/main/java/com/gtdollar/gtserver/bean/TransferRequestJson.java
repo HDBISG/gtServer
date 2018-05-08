21aa8337dfb838791eb07df8ac7ecb5c84043291
ok <zhangji@hotmail.com>
2018-05-08 22:37:05 +0800

    add transfer and enquiry transfer API

---
+++ src/main/java/com/gtdollar/gtserver/bean/TransferRequestJson.java
@@ -0,0 +1,58 @@
+package com.gtdollar.gtserver.bean;
+
+import com.gtdollar.gtserver.model.Account;
+import com.gtdollar.gtserver.model.Transfer;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by ok on 8/5/18.
+ */
+
+public class TransferRequestJson {
+    String email;
+    String transferee;
+    BigDecimal amount;
+
+    @Override
+    public String toString() {
+        return "TransferRequestJson{" +
+                "email='" + email + '\'' +
+                ", transferee='" + transferee + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+
+    public void toTransfer(Transfer transfer) {
+
+        transfer.setTranAmount( getAmount() );
+        transfer.setOtherEmail( getTransferee() );
+        Account account = new Account();
+        account.setEmail( getEmail() );
+        transfer.setAccount( account );
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTransferee() {
+        return transferee;
+    }
+
+    public void setTransferee(String transferee) {
+        this.transferee = transferee;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+}
